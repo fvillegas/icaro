dbfd5122dcea5df2c05444182efdd78ff6025440
Francisco Villegas <fcvillegas@gmail.com>
2020-06-20 19:15:38 -0300

    main.c display raw values

--- rpi-poc/main.c
+++ rpi-poc/main.c
@@ -3,6 +3,14 @@
 
 int main(void)
 {
+    int16_t ax, ay, az, gx, gy, gz;
     mpu6050_initialize();
+
+    while(1)
+    {
+      get_motion_6(&ax, &ay, &az, &gx, &gy, &gz);
+      printf("%d,%d,%d,%d,%d,%d\n", ax, ay, az, gx, gy, gz);
+    }
+
     return 0;
 }
